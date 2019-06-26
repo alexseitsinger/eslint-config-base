28809a4ca76ad5be2c60032079a92636f179188d
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-26 13:48:44 -0400

    feat: Enables no-unused-modules rule.

--- src/rules/import/lib/static-analysis.js
+++ src/rules/import/lib/static-analysis.js
@@ -186,24 +186,95 @@ module.exports = {
 
     /**
      * Forbid modules without any export, and exports not imported by any
-     * modules..
+     * modules.
      *
      * NOTES:
-     * - We have unused exports in webpack configs, we disable this rule.
+     * - We always have unused exports in project and package entry points, as
+     *   well as javascript config files. So, we add them to the ignoredExports
+     *   list.
+     * - missingExports doesnt count module.exports so disable that part of the
+     *   rule.
      *
      * https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unused-modules.md
      *
      * @example
-     * ["off", {
+     * ["error", {
      *  unusedExports: true,
      *  missingExports: true,
+     *  ignoreExports: [
+     *    "index.js",
+     *    "babel.config.js",
+     *    "jest.config.js",
+     *    "jest.setup.js",
+     *    "jest.assetTransformer.js",
+     *    ".eslintrc.js",
+     *    "webpack.config.js",
+     *    "webpack-config.js",
+     *    "webpack-config.client.development.js",
+     *    "webpack-config.client.staging.js",
+     *    "webpack-config.client.production.js",
+     *    "webpack-config.server.development.js",
+     *    "webpack-config.server.staging.js",
+     *    "webpack-config.server.production.js",
+     *    "webpack/options.js",
+     *    "webpack/base.js",
+     *    "webpack/development.base.js",
+     *    "webpack/production.base.js",
+     *    "webpack/staging.base.js",
+     *    "webpack/client/base.js",
+     *    "webpack/client/development.js",
+     *    "webpack/client/staging.js",
+     *    "webpack/client/production.js",
+     *    "webpack/client/shared.base.js",
+     *    "webpack/server/base.js",
+     *    "webpack/server/development.js",
+     *    "webpack/server/staging.js",
+     *    "webpack/server/production.js",
+     *    "webpack/server/shared.base.js",
+     *    "src/index.js",
+     *    "src/server.js",
+     *    "src/client.js",
+     *  ]
      * }]
      */
-    "import/no-unused-modules": ["off", {
-      unusedExports: true,
-      missingExports: true,
+    "import/no-unused-modules": ["error", {
       //src: [],
-      //ignoreExports: [],
+      missingExports: false,
+      unusedExports: true,
+      ignoreExports: [
+        "babel.config.js",
+        ".eslintrc.js",
+        "jest.config.js",
+        "jest.setup.js",
+        "jest.assetTransformer.js",
+        "webpack.config.js",
+        "webpack-config.js",
+        "webpack-config.client.development.js",
+        "webpack-config.client.staging.js",
+        "webpack-config.client.production.js",
+        "webpack-config.server.development.js",
+        "webpack-config.server.staging.js",
+        "webpack-config.server.production.js",
+        "webpack/options.js",
+        "webpack/base.js",
+        "webpack/development.base.js",
+        "webpack/production.base.js",
+        "webpack/staging.base.js",
+        "webpack/client/development.js",
+        "webpack/client/production.js",
+        "webpack/client/staging.js",
+        "webpack/client/shared.base.js",
+        "webpack/client/base.js",
+        "webpack/server/development.js",
+        "webpack/server/production.js",
+        "webpack/server/staging.js",
+        "webpack/server/shared.base.js",
+        "webpack/server/base.js",
+        "index.js",
+        "src/index.js",
+        "src/server.js",
+        "src/client.js",
+      ],
     }],
   }
 }
